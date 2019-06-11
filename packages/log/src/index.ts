f0890ebef102690d2307d6c3eee8884dd3c931c8
Steve Korshakov <steve@korshakov.com>
2019-06-10 21:40:44 -0700

    fix: Fix exports

--- packages/log/src/index.ts
+++ packages/log/src/index.ts
@@ -3,3 +3,7 @@ export { Logger } from './Logger';
 export { enableAll, disableAll, enableService, disableService, setLogProvider } from './config';
 export { LogProvider } from './LogProvider';
 export { ConsoleLogProvider } from './ConsoleLogProvider';
+export { LogPathContext } from './LogPathContext';
+export { LogMetaContext } from './LogMetaContext';
+export { withLogMeta } from './withLogMeta';
+export { withLogPath } from './withLogPath';
